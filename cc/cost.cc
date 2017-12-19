63f65533ef0c09b181b57c6fd3c61385b906c568
Li Ji <39690040+liji-heptax@users.noreply.github.com>
2017-12-20 01:31:28 +0800

    separate the file io functions

--- cc/cost.cc
+++ cc/cost.cc
@@ -1,52 +1,17 @@
 #include "cost.h"
-#include "cJSON.h"
+#include "fileio.h"
 #include <iostream>
-#include <fstream>
+#include <cstdlib>
 
 using namespace std;
 
-namespace
-{
-  char *_file2str(const char *filename, size_t &len)
-  {
-    ifstream ifs (filename, ifstream::binary);
-    if (!ifs)
-    {
-      cerr << "Cannot open file!\n" << "Error code: " << strerror(errno);
-      return 0;
-    }
-    // get pointer to associated buffer object
-    filebuf *pbuf = ifs.rdbuf();
-    // get file size using buffer's members
-    size_t size = pbuf->pubseekoff (0,ifs.end,ifs.in);
-    pbuf->pubseekpos (0,ifs.in);
-    // allocate memory to contain file data
-    char *buffer = new char[size];
-    // get file data
-    pbuf->sgetn (buffer,size);
-    ifs.close();
-    len = size;
-    return buffer;
-  }
-
-  int _parse_cost_json_file(const char *cost_file)
-  {
-    size_t len = 0;
-    char *str = _file2str(cost_file, len) ;
-    delete[] str;
-    cJSON *json = cJSON_Parse(str);
-    if (!json)
-    {
-      cout <<  "Error before: \n"<< endl ;
-      return -1;
-    }
-    return 0;
-  }
-}
-
 CostMatrix::CostMatrix(const char *filename)
 {
-  _parse_cost_json_file(filename);
+  cJSON *json = parse_json_file(filename);
+  const char *out = cJSON_Print(json);
+  cJSON_Delete(json);
+  cout << out << endl;
+  free(const_cast<char *>(out));
 }
 
 CostMatrix::~CostMatrix()
---
+++ cc/fileio.h
@@ -0,0 +1,8 @@
+#ifndef __FILEIO_H__
+#define __FILEIO_H__
+
+#include "cJSON.h"
+
+cJSON *parse_json_file(const char *filename);
+
+#endif // __FILEIO_H__
