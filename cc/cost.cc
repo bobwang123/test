1a6c98627fa5f8a02ecc7589b174b9ddf04ecd76
Li Ji <39690040+liji-heptax@users.noreply.github.com>
2017-12-20 17:18:08 +0800

    implement cost matrix parser

--- cc/cost.cc
+++ cc/cost.cc
@@ -32,7 +32,8 @@ CostMatrix::_create_city_indices(cJSON *json)
   for (cJSON *city = json_city_idx->child; city; city = city->next)
   {
     pair<map<string, CityIdxType>::iterator, bool> ret =
-      _city_indices.insert(pair<string, CityIdxType>(city->string, city->valueint));
+      _city_indices.insert(
+        pair<string, CityIdxType>(city->string, city->valueint));
     if (!ret.second)
     {
       cerr << "**Error: duplicated city " << city->string << " found!" << endl;
@@ -48,9 +49,50 @@ CostMatrix::_create_city_indices(cJSON *json)
   return _city_indices;
 }
 
-std::map<std::string, Cost *> **
+CostMatrix::_BaseCostMapType **
 CostMatrix::_create_cost_mat(cJSON *json)
 {
+  const size_t ndim = _cities.size();
+  _cost_mat = new _BaseCostMapType *[ndim];
+  for (int i = 0; i < ndim; ++i)
+    _cost_mat[i] = new _BaseCostMapType[ndim];
+  cJSON *json_2d = cJSON_GetObjectItem(json, "cost_matrix");
+  assert(ndim == cJSON_GetArraySize(json_2d));
+  vsp_debug && cout << "\nCost Matrix\n" << "[" << "\n";
+  for (int i = 0; i < ndim; ++i)
+  {
+    vsp_debug && cout << " [" << "\n";
+    cJSON *json_1d = cJSON_GetArrayItem(json_2d, i);
+    assert(ndim == cJSON_GetArraySize(json_1d));
+    for (int j = 0; j < ndim; ++j)
+    {
+      vsp_debug && cout << "  {";
+      cJSON *route_cost_map = cJSON_GetArrayItem(json_1d, j);
+      for (cJSON *route_cost = route_cost_map->child;
+           route_cost; route_cost = route_cost->next)
+      {
+        const char *route_name = route_cost->string;
+        double dist = cJSON_GetObjectItem(route_cost, "distance")->valuedouble;
+        double expe = cJSON_GetObjectItem(route_cost, "expense")->valuedouble;
+        double dura = cJSON_GetObjectItem(route_cost, "duration")->valuedouble;
+        vsp_debug && cout << "\"" << route_name << "\": "
+          << "{" << "\"distance\": " << dist << "," << "\"expense\": "  << expe
+          << "," << "\"duration\": " << dura << "}, ";
+        Cost cst(dist, expe, dura);
+        pair<map<string, Cost>::iterator, bool> ret =
+          _cost_mat[i][j].insert(pair<string, Cost>(route_name, cst));
+        if (!ret.second)
+        {
+          cerr << "**Error: duplicated route name "
+            << route_cost->string << " found!" << endl;
+          exit(-1);
+        }
+      }
+      vsp_debug && cout << "}," << "\n";
+    }
+    vsp_debug && cout << " ]," << "\n";
+  }
+  vsp_debug && cout << "]" << "\n";
   return _cost_mat;
 }
 
@@ -62,23 +104,23 @@ CostMatrix::_create_prob_mat(cJSON *json)
   for (int i = 0; i < ndim; ++i)
     _prob_mat[i] = new _BaseProbArrayType[ndim];
   cJSON *json_3d = cJSON_GetObjectItem(json, "prob_matrix");
-  vsp_debug && cout << "[" << "\n";
+  vsp_debug && cout << "\nProbability Matrix\n" << "[" << "\n";
   for (int i = 0; i < cJSON_GetArraySize(json_3d); ++i)
   {
-    vsp_debug && cout << "\t[" << "\n";
+    vsp_debug && cout << " [" << "\n";
     cJSON *json_2d = cJSON_GetArrayItem(json_3d, i);
     for (int j = 0; j < cJSON_GetArraySize(json_2d); ++j)
     {
-      vsp_debug && cout << "\t\t[";
+      vsp_debug && cout << "  [";
       cJSON *json_1d = cJSON_GetArrayItem(json_2d, j);
       for (int k = 0; k < cJSON_GetArraySize(json_1d); ++k)
       {
         _prob_mat[i][j][k] = cJSON_GetArrayItem(json_1d, k)->valuedouble;
         vsp_debug && cout << _prob_mat[i][j][k] << ", ";
       }
-      vsp_debug && cout << "\t\t]," << "\n";
+      vsp_debug && cout << "]," << "\n";
     }
-    vsp_debug && cout << "\t]," << "\n";
+    vsp_debug && cout << " ]," << "\n";
   }
   vsp_debug && cout << "]" << "\n";
   return _prob_mat;
@@ -105,7 +147,12 @@ CostMatrix::~CostMatrix()
 {
   const size_t ndim = _cities.size();
   for (int i = 0; i < ndim; ++i)
+  {
+    delete[] _cost_mat[i];
     delete[] _prob_mat[i];
+  }
+  delete[] _cost_mat;
+  _cost_mat = 0;
   delete[] _prob_mat;
   _prob_mat = 0;
 }
--- cc/cost.h
+++ cc/cost.h
@@ -8,25 +8,41 @@
 
 class Cost
 {
-  const double _distance;
-  const double _expense;
-  const double _duration;
+  double _distance;
+  double _expense;
+  double _duration;
 public:
   Cost(const double distance=0.0,
-       const double expense=0.0,
+       const double expense =0.0,
        const double duration=0.0)
-    :_distance(distance), _expense(expense), _duration(duration) {}
-  ~Cost() {}
+    : _distance(distance), _expense(expense), _duration(duration)
+  {}
+  Cost(const Cost &cst)
+    : _distance(cst._distance), _expense(cst._expense), _duration(cst._duration)
+  {}
+  ~Cost()
+  {}
 public:
   const double distance() const { return _distance; }
   const double expense()  const { return _expense;  }
   const double duration() const { return _duration; }
+  Cost &operator=(const Cost &cst)
+  {
+    if (this != &cst)
+    {
+      _distance = cst._distance;
+      _expense  = cst._expense;
+      _duration = cst._duration;
+    }
+    return *this;
+  }
 };
 
 class CostMatrix
 {
   // each hour each distribution per day
   static const std::size_t _NUM_PROB_TICKS = 24;
+  typedef std::map<std::string, Cost> _BaseCostMapType;
   typedef double _BaseProbArrayType[_NUM_PROB_TICKS];
 public:
   typedef std::vector<std::string>::size_type CityIdxType;
@@ -36,7 +52,7 @@ private:
   // [city_name] -> CityIdxType
   std::map<std::string, CityIdxType> _city_indices;
   // [from][to][route_name] -> Cost
-  std::map<std::string, Cost *> **_cost_mat;
+  _BaseCostMapType **_cost_mat;
   // [from][to][hour_tick] -> double
   _BaseProbArrayType **_prob_mat;
 public:
@@ -45,7 +61,7 @@ public:
 private:
   std::vector<std::string> &_create_cities(cJSON *json);
   std::map<std::string, CityIdxType> &_create_city_indices(cJSON *json);
-  std::map<std::string, Cost *> **_create_cost_mat(cJSON *json);
+  _BaseCostMapType **_create_cost_mat(cJSON *json);
   _BaseProbArrayType **_create_prob_mat(cJSON *json);
   int _parse_cost_json(cJSON *json);
 };
