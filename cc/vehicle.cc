58586fc52d796ad54f4d8fb6d5b4ab52000022dc
Li Ji <39690040+liji-heptax@users.noreply.github.com>
2017-12-22 18:59:11 +0800

    fix free unmalloced issue

--- cc/vehicle.cc
+++ cc/vehicle.cc
@@ -1,9 +1,14 @@
 #include "vehicle.h"
 #include "task.h"
 #include "route.h"
+#include "plan.h"
 
-Vehicle::Vehicle(const char *name, CostMatrix::CityIdxType avl_loc,
-                 const double avl_time=0.0, const int candidate_num_limit=10)
+using namespace std;
+
+Vehicle::Vehicle(const string &name,
+                 const CostMatrix::CityIdxType avl_loc,
+                 const double avl_time,
+                 const int candidate_num_limit)
   : _name(name), _avl_loc(avl_loc), _avl_time(avl_time),
   _candidate_num_limit(candidate_num_limit)
 {
@@ -14,8 +19,13 @@ Vehicle::Vehicle(const char *name, CostMatrix::CityIdxType avl_loc,
 
 Vehicle::~Vehicle()
 {
-  delete[] _candidate_plans_sorted;
+  // delete[] _candidate_plans_sorted;
+  _candidate_plans_sorted = 0;
   delete _start_route;
+  _start_route = 0;
   delete _start_task;
+  _start_task = 0;
   delete _start_cost;
+  _start_cost = 0;
 }
+
