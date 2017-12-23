efca213192bc0f8d6c7e029d87a1cc97b7fe2965
Li Ji <39690040+liji-heptax@users.noreply.github.com>
2017-12-23 21:21:53 +0800

    remove redundant inline key words

--- cc/consts.h
+++ cc/consts.h
@@ -6,9 +6,9 @@ class Consts
   static const double _DOUBLE_MAX;
 public:
   static const double DOUBLE_NONE;
-  static inline bool is_none(const double v) { return v < -_DOUBLE_MAX; }
+  static bool is_none(const double v) { return v < -_DOUBLE_MAX; }
   static const double DOUBLE_INF;
-  static inline bool is_inf(const double v) { return _DOUBLE_MAX < v; }
+  static bool is_inf(const double v) { return _DOUBLE_MAX < v; }
 };
 
 #endif /* __CONSTS_H__ */
