450224bd9645497033dc595ed1aee4b6a9e27e40
Li Ji <39690040+liji-heptax@users.noreply.github.com>
2017-12-25 13:58:52 +0800

    use reasonable cJSON moving subtree operation

--- cc/plan.h
+++ cc/plan.h
@@ -18,8 +18,8 @@ class Plan
          cit != _steps.end(); ++cit)
     {
       cJSON *empty_and_order = (*cit)->to_dict(cost_prob_mat);
-      cJSON *empty = cJSON_Duplicate(cJSON_GetArrayItem(empty_and_order, 0), 0);
-      cJSON *order = cJSON_Duplicate(cJSON_GetArrayItem(empty_and_order, 1), 0);
+      cJSON *empty = cJSON_DetachItemFromArray(empty_and_order, 0);
+      cJSON *order = cJSON_DetachItemFromArray(empty_and_order, 1);
       cJSON_Delete(empty_and_order);  // make sure no memory leak
       cJSON_AddItemToArray(step_arr, empty);
       cJSON_AddItemToArray(step_arr, order);
