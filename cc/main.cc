38794f3c11fe629f24defd500c901bcd6f02a0a7
Li Ji <39690040+liji-heptax@users.noreply.github.com>
2017-12-26 20:01:48 +0800

    Use multi-process to by pass the destructor performance issues

--- cc/main.cc
+++ cc/main.cc
@@ -2,6 +2,7 @@
 #include "cost.h"
 #include "timer.h"
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
@@ -11,8 +12,39 @@ extern const int vsp_debug = 1;
 extern const int vsp_debug = 0;
 #endif  // DEBUG
 
-int main()
+extern int
+multi_proc_main(const char *api, Scheduler *sh);
+
+static const char *_OUTPUT_PLAN_FILE = "cxx_outplans.json";
+
+void
+compute(Scheduler *sh)
 {
+  double t1 = get_wall_time();
+  sh->run();
+  print_wall_time_diff(t1, "Total Scheduler::run");
+
+  t1 = get_wall_time();
+  sh->dump_plans(_OUTPUT_PLAN_FILE);
+  print_wall_time_diff(t1, "Total Scheduler::dump_plans");
+}
+
+void
+upload(const char *api)
+{
+  string cmd = "curl --output curl.log --include --silent --show-error --form resultsStr=<";
+  cmd += _OUTPUT_PLAN_FILE;
+  cmd += ";type=text/plain ";
+  cmd += api;
+  // TODO: system(cmd.c_str());
+  cout << cmd << endl;
+}
+
+int main(int argc, char *argv[])
+{
+  const char *upload_api = argc > 2 ? argv[2] : static_cast<const char *>(0);
+  if (!upload_api)
+    cout << "** Warning: No plan is uploaded because API is invalid." << endl;
   double tg1 = get_wall_time();
   double t1 = get_wall_time();
   // Generate this file using "$ python python/cost.py cost.json prob.json"
@@ -20,17 +52,18 @@ int main()
   print_wall_time_diff(t1, "Total init CostMatrix");
 
   t1 = get_wall_time();
-  Scheduler sh("vehicles.http_api.json", "orders.http_api.json", cm, 0);
+  Scheduler sh("vehicles.http_api.json", "orders.http_api.json", cm);
   print_wall_time_diff(t1, "Total init Scheduler");
 
-  t1 = get_wall_time();
-  sh.run();
-  print_wall_time_diff(t1, "Total Scheduler::run");
-
-  t1 = get_wall_time();
-  sh.dump_plans("cxx_outplans.json");
-  print_wall_time_diff(t1, "Total Scheduler::dump_plans");
-
+  if (argc > 1 && argv[1][0] == '1')
+  {
+    multi_proc_main(upload_api, &sh);
+  }
+  else  // single process
+  {
+    compute(&sh);
+    upload(upload_api);
+  }
   print_wall_time_diff(tg1, "Total");
   return 0;
 }
---
+++ cc/multi_proc.cc
@@ -0,0 +1,78 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+class Scheduler;
+
+extern void
+compute(Scheduler *sh);
+
+extern void
+upload(const char *api);
+
+/* Read characters from the pipe and echo them to stdout. */
+
+void
+read_from_pipe (int file)
+{
+  FILE *stream;
+  int c;
+  stream = fdopen (file, "r");
+  while ((c = fgetc (stream)) != EOF)
+    putchar (c);
+  fclose (stream);
+}
+
+/* Write some random text to the pipe. */
+
+void
+write_to_pipe (int file)
+{
+  FILE *stream;
+  stream = fdopen (file, "w");
+  fprintf (stream, "Done!\n");
+  fclose (stream);
+}
+
+int
+multi_proc_main(const char *api, Scheduler *sh)
+{
+  pid_t pid;
+  int mypipe[2];
+
+  /* Create the pipe. */
+  if (pipe (mypipe))
+  {
+    fprintf (stderr, "Pipe failed.\n");
+    return EXIT_FAILURE;
+  }
+
+  /* Create the child process. */
+  pid = fork ();
+  if (pid == (pid_t) 0)
+  {
+    /* This is the child process.
+       Close other end first. */
+    close (mypipe[0]);
+    compute(sh);
+    write_to_pipe (mypipe[1]);
+    return EXIT_SUCCESS;
+  }
+  else if (pid < (pid_t) 0)
+  {
+    /* The fork failed. */
+    fprintf (stderr, "Fork failed.\n");
+    return EXIT_FAILURE;
+  }
+  else
+  {
+    /* This is the parent process.
+       Close other end first. */
+    close (mypipe[1]);
+    read_from_pipe (mypipe[0]);
+    upload(api);
+    return EXIT_SUCCESS;
+  }
+}
+
