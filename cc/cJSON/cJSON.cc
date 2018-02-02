a33c8f7798554cfd91fc2ebd7b99ec31e7d9e269
Li Ji <39690040+liji-heptax@users.noreply.github.com>
2018-02-02 18:00:31 +0800

    fix cJSON dumping issue when < 1e-17(i.e. DBL_EPSILON)

--- cc/cJSON/cJSON.cc
+++ cc/cJSON/cJSON.cc
@@ -154,7 +154,7 @@ static char *print_number(cJSON *item,printbuffer *p)
 		else	str=(char*)cJSON_malloc(2);	/* special case for 0. */
 		if (str) strcpy(str,"0");
 	}
-	else if (fabs(((double)item->valueint)-d)<=DBL_EPSILON && d<=INT_MAX && d>=INT_MIN)
+	else if (fabs(((double)item->valueint)-d)<=DBL_MIN && d<=INT_MAX && d>=INT_MIN)
 	{
 		if (p)	str=ensure(p,21);
 		else	str=(char*)cJSON_malloc(21);	/* 2^64+1 can be represented in 21 chars. */
@@ -166,7 +166,7 @@ static char *print_number(cJSON *item,printbuffer *p)
 		else	str=(char*)cJSON_malloc(64);	/* This is a nice tradeoff. */
 		if (str)
 		{
-			if (fabs(floor(d)-d)<=DBL_EPSILON && fabs(d)<LLONG_MAX) sprintf(str,"%lld",(long long)d);
+			if (fabs(floor(d)-d)<=DBL_MIN && fabs(d)<LLONG_MAX) sprintf(str,"%lld",(long long)d);
 			else if (fabs(d)<1.0e-6 || fabs(d)>1.0e9) sprintf(str,"%e",d);
 			else sprintf(str,"%f",d);
 		}
