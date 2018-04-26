b0d9c57bc58bd4763828daf4b5179010f3c4e97c
Li Ji <39690040+liji-heptax@users.noreply.github.com>
2018-04-26 20:49:02 +0800

    speed up launching process using make -j multi-processing

--- launch_web_server.sh
+++ launch_web_server.sh
@@ -1,1 +1,1 @@
-make --directory cc && make --directory python && node web_server.js 2>&1 | tee -a web_server.log
+make -j --directory cc && make -j --directory python && node web_server.js 2>&1 | tee -a web_server.log
