336b56f638e402a9ec1d1a49565e5a3fbcea966b
Li Ji <39690040+liji-heptax@users.noreply.github.com>
2018-05-17 17:15:25 +0800

    add cache file to speed up the MDP procedure by reusing the init values

--- rec/mdp_risk_refined.py
+++ rec/mdp_risk_refined.py
@@ -32,7 +32,7 @@ num_risk_level = len(risk_level)
 
 # basic configurations/parameters among those cities.
 city_distance = np.zeros([num_city, num_city])
-city_time = np.zeros([num_city, num_city])
+#city_time = np.zeros([num_city, num_city])
 city_cost = np.zeros([num_city, num_city])
 avg_order_num = np.zeros([num_city, num_city])
 std_order_num = np.zeros([num_city, num_city])
@@ -49,10 +49,10 @@ trans_matrix = np.ones([num_city, num_city, num_city, num_city])
 #  1. for the time being, reward is kind of an expected value according to the status of the network: price, distance, cost and etc.
 #  2. in the future, reward can include the time factor, e.g., reward/hour or reward/day.
 
-order_reward = np.zeros([num_city, num_city])
+#order_reward = np.zeros([num_city, num_city])
 order_prob = np.zeros([num_city, num_city])
 reward = np.zeros([num_city, num_city, num_city, num_risk_level])
-reward_per_hour = np.zeros([num_city, num_city, num_city, num_risk_level])
+#reward_per_hour = np.zeros([num_city, num_city, num_city, num_risk_level])
 
 # In[30]:
 
@@ -137,13 +137,25 @@ order_reward = avg_price - city_cost
 for c in range(num_city):
     for from_city in range(num_city):
         for to_city in range(num_city):
+            city_time_empty = city_time[c][from_city]
+            city_time_run = city_time[from_city][to_city]
+            order_rwd = order_reward[from_city][to_city]
+            city_cst = city_cost[c][from_city]
+            prob = order_prob[from_city][to_city]
             for risk in range(num_risk_level):
+                duration = city_time_empty + city_time_run + upload_time + download_time
+                time_cost = duration * fix_cost_per_hour
+                rwd = (order_rwd - city_cst - time_cost) * np.power(prob, 1 - risk_level[risk])
+                reward[c, from_city, to_city, risk] = rwd
+                #reward_per_hour[c, from_city, to_city, risk] = rwd / duration
+                """
                 time_cost = (city_time[c][from_city] + city_time[from_city][
                     to_city] + upload_time + download_time) * fix_cost_per_hour
                 reward[c, from_city, to_city, risk] = (order_reward[from_city][to_city] - city_cost[c][
                     from_city] - time_cost) * np.power(order_prob[from_city][to_city], 1 - risk_level[risk])
                 reward_per_hour[c, from_city, to_city, risk] = reward[c, from_city, to_city, risk] / (
                 city_time[c][from_city] + city_time[from_city][to_city] + upload_time + download_time)
+                """
 
 # print(order_prob)
 # print(city_distance)
@@ -184,11 +196,12 @@ def value_iteration(num_city, util_amplifier, epslon, alpha, gamma, reward,
                     utility=np.random.rand(num_city, num_city, num_city),
                     trans_matrix=np.ones([num_city, num_city, num_city, num_city]), max_iter=1000):
     count = 0
-    error = 1
+    error2 = 1.0
     new_utility = np.zeros_like(utility)
     U_max = np.zeros(num_city)
+    epslon2 = epslon * epslon
 
-    while error > epslon and count < max_iter:
+    while error2 > epslon2 and count < max_iter:
         for s in range(num_city):
             U_max[s] = max(max(utility[s, :, :].reshape(1, num_city * num_city)))
 
@@ -197,12 +210,12 @@ def value_iteration(num_city, util_amplifier, epslon, alpha, gamma, reward,
             for from_city in range(num_city):
                 for to_city in range(num_city):
                     new_utility[s][from_city][to_city] = (1 - alpha) * utility[s][from_city][to_city] + alpha * (
-                    reward[s][from_city][to_city] + gamma * U_max[to_city])
-        error = np.sqrt(sum(sum(np.square(new_utility - utility).reshape(1, num_city * num_city * num_city))))
+                            reward[s][from_city][to_city] + gamma * U_max[to_city])
+        error2 = sum(sum(np.square(new_utility - utility).reshape(1, num_city * num_city * num_city)))
         utility = new_utility.copy()
         count += 1
     print("total number of iteration: ", count)
-    print("final error: ", error)
+    print("final error2: ", error2)
     return new_utility
 
 
@@ -248,13 +261,20 @@ alpha = 0.7
 gamma = 0.9
 max_iter = 1000
 # count = 0
+utility_cache_file = 'utility_cache.npy'
 
-utility = np.random.rand(num_city, num_city, num_city, num_risk_level)
-# utility = np.zeros([num_city, num_city, num_city])
+utility = np.zeros([num_city, num_city, num_city, num_risk_level])
+# read from latest cache if existing and having the same shape
+try:
+    utility = np.load(utility_cache_file)
+except IOError:
+    utility = np.random.rand(num_city, num_city, num_city, num_risk_level)
 for i in range(num_risk_level):
     print("Risk preference level:", risk_level[i])
     utility[:, :, :, i] = value_iteration(num_city, util_amplifier, epslon, alpha, gamma, reward[:, :, :, i],
                                           utility[:, :, :, i], trans_matrix, max_iter)
+# make a backup (cache) for future reuse
+np.save(utility_cache_file, utility)
 
 
 # In[12]:
@@ -267,6 +287,8 @@ def insert_into_db(result_list):
 sql_clean = "truncate TABLE q_value"
 sqlcmd.executeSql(sql_clean)
 
+# !!! use risk level and city as command line option
+# !!! by default use all risk levels and all cities
 print("---------------------------------------------------")
 for s in range(num_city):
     print("-----------------")
@@ -275,7 +297,7 @@ for s in range(num_city):
         result_list = search_policy(risk_level[r],utility[:, :, :, r], id2city[s], city2id, city_distance,
                                     max_empty_distance=200, top=10)
         print(result_list)
-        #insert_into_db(result_list)
+        # insert_into_db(result_list)
 print("---------------------------------------------------")
 
 # In[38]:
