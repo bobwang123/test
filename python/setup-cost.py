545a65aebfd81cb57a92be6d31177e067431b6f8
Li Ji <39690040+liji-heptax@users.noreply.github.com>
2018-06-12 16:57:06 +0800

    build py .so parallel

---
+++ python/setup-cost.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+from distutils.extension import Extension
+# noinspection PyPackageRequirements
+from Cython.Distutils import build_ext
+ext_modules = [
+        Extension("cost", ["cost.py"]),
+        ]
+setup(
+        name='Zhicang logistics package',
+        cmdclass={'build_ext': build_ext},
+        ext_modules=ext_modules
+        )
---
+++ python/setup-scheduler.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+from distutils.extension import Extension
+# noinspection PyPackageRequirements
+from Cython.Distutils import build_ext
+ext_modules = [
+        Extension("scheduler", ["scheduler.py"])]
+setup(
+        name='Zhicang logistics package',
+        cmdclass={'build_ext': build_ext},
+        ext_modules=ext_modules
+        )
